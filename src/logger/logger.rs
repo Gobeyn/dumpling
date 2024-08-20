956e907348ddc4abcee4b638515982bb269248bf
Aaron Gobeyn <aaron.gobeyn@gmail.com>
2024-08-20 08:52:47 +0200

    Added which file and which line an error came from in the logger that is documented in the log file.

--- src/logger/logger.rs
+++ src/logger/logger.rs
@@ -19,9 +19,11 @@ pub fn init_logging() {
             let mut log_file = log_file.lock().unwrap();
             writeln!(
                 log_file,
-                "{} [{}] - {}",
+                "{} [{}] - {}:{} - {}",
                 chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
                 record.level(),
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
                 record.args()
             )
         })
